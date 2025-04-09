4058ff5a15dc3db83b6795f782e6224c5f661986
cassianobraz <cassanopb@gmail.com>
2025-04-09 19:49:35 -0400

    first commit

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,14 @@
+﻿namespace CashFlow.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public List<string> ErrorMessages { get; set; }
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessages = [errorMessage];
+    }
+    public ResponseErrorJson(List<string> errorMessage)
+    {
+        ErrorMessages = errorMessage;
+    }
+}
